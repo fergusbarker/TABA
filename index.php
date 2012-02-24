0f467add13e3686f0c292efdfa2b59997a1961eb
unknown <gaming@gaming-PC.(none)>
2012-02-24 18:43:27 +1100

    i am mik

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+	<title> There and Back Again </title>
+	<link href='http://fonts.googleapis.com/css?family=Fondamento' rel='stylesheet' type='text/css'>
+	<link rel='Stylesheet' type='text/css' HREF = 'style.css'>
+
+</head>
+
+<body>
+<blockquote>
+<p>
+"Chip the glasses and crack the plates!
+Blunt the knives and bend the forks!
+That's what Bilbo hates -
+Smash the bottles and burn the corks!
+
+Cut the cloth and tread on the fat!
+Pour the milk on the pantry floor!
+Leave the bones on the bedroom mat!
+Splash the wine on every door!"
+</p>
+</blockquote>
+</body>
+
+</html>
