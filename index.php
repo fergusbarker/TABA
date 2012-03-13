7af2249792022138ccef17ad976a7082ce3770ff
Michael <Michael@Michael-BEAST.(none)>
2012-03-13 19:29:08 +1100

    Changed delimiter to "|" instead of ","

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 	$arrCSV = array();
 	if (($handle = fopen("sentences.csv", "r")) !==FALSE) {
  		$key = 0;
-		while (($data = fgetcsv($handle , 0, ",")) ! ==FALSE) {
+		while (($data = fgetcsv($handle , 0, "|")) ! ==FALSE) {
 			$c = count($data)
 			for ($x=0;$x<$c;$x++) {
 				$arrCSV[$key][$x] = $data[$x];
