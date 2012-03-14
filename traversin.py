31f9f067e469067b70295fe3761ad56c6d814812
Fergus Barker <fergusbarker@gmail.com>
2012-03-14 15:06:54 +1100

    Added a function to contain it. Improved comments and readability.

--- traversin.py
+++ traversin.py
@@ -1,21 +1,31 @@
+#!/usr/bin/env python
+# For generating sentence split CSV files from plain text
 import csv
+
+# Main function
+def gencsv(file):
+    
+    # Opens the file and splits it
+    array = file.read()
+    array = array.split(".")
+    
+    # Closes the file, only working from array now
+    file.close()
+    
+    # Creating CSV
+    newcsv = opem('sentences.csv', 'w')
+    writer = csv.writer(newcsv, delimiter='|')
+    
+    # Writing CSV
+    writer.writerow(array)
+    
+    # Closing
+    newcsv.close()
+
 
 hobbit = open("thehobbit.txt", 'r')
 
-y = hobbit.read()
+gencsv(hobbit)
 
-y = y.split(".")
-
-for i in y:
-	
-    print i + "."
-
-hobbit.close()
-
-newcsv = open('sentences.csv','w')
-writer = csv.writer(newcsv, delimiter="|")
-
-writer.writerow(y)
-
-# Goonies forever
+# Stops closing on some systems
 raw_input()
